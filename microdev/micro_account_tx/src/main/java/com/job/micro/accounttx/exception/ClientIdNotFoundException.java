0ce620a207640bea7f4d898093f2f28d22010021
edu <vizuete.eduardo@gmail.com>
2024-12-05 10:24:16 -0500

    Exceptions: delete ResponseStatus from customized exceptions

--- microdev/micro_account_tx/src/main/java/com/job/micro/accounttx/exception/ClientIdNotFoundException.java
+++ microdev/micro_account_tx/src/main/java/com/job/micro/accounttx/exception/ClientIdNotFoundException.java
@@ -1,9 +1,5 @@
 package com.job.micro.accounttx.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ClientIdNotFoundException extends RuntimeException {
 
     public ClientIdNotFoundException(String message) {
--- microdev/micro_person_client/src/main/java/com/job/micro/personclient/exception/PersonIdentAlreadyExistsException.java
+++ microdev/micro_person_client/src/main/java/com/job/micro/personclient/exception/PersonIdentAlreadyExistsException.java
@@ -1,9 +1,5 @@
 package com.job.micro.personclient.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class PersonIdentAlreadyExistsException extends RuntimeException {
 
     public PersonIdentAlreadyExistsException(String message) {
