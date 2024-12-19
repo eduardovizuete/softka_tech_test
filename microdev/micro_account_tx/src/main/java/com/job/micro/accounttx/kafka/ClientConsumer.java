6391f886985b281a7168172b7ae00a2267ca39c7
edu <vizuete.eduardo@gmail.com>
2024-12-19 11:53:32 -0500

    feature: kafka - publisher and subscriber topic

---
+++ microdev/micro_account_tx/src/main/java/com/job/micro/accounttx/kafka/ClientConsumer.java
@@ -0,0 +1,20 @@
+package com.job.micro.accounttx.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientConsumer.class);
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(String event) {
+        LOGGER.info("Client event received in microat service => {}", event);
+    }
+
+}
