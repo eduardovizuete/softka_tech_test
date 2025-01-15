14600ee41771aa57e16f7308757309ae21c850d4
edu <vizuete.eduardo@gmail.com>
2025-01-15 12:26:03 -0500

    refactor: refactor log test and pom dependencies

--- microdev/api-gateway/src/test/java/com/job/micro/apigateway/ApiGatewayApplicationTests.java
+++ microdev/api-gateway/src/test/java/com/job/micro/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,18 @@
 package com.job.micro.apigateway;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ApiGatewayApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOG.info("Running contextLoad test...");
 	}
 
 }
--- microdev/service-registry/src/test/java/com/job/micro/service_registry/ServiceRegistryApplicationTests.java
+++ microdev/service-registry/src/test/java/com/job/micro/service_registry/ServiceRegistryApplicationTests.java
@@ -1,13 +1,18 @@
 package com.job.micro.service_registry;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceRegistryApplicationTests {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ServiceRegistryApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOG.info("Running contextLoad test...");
 	}
 
 }
